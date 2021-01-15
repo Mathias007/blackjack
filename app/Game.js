4580d61519c3e3de1e0d654079ac893d583e7917
Mathias007 <mathias.najlepszy@gmail.com>
2021-01-15 20:58:35 +0100

    create Game class, add constructor, run(), dealCards()

--- app/Game.js
+++ app/Game.js
@@ -1,7 +1,43 @@
 import { Player } from "./Player.js";
 import { Deck } from "./Deck.js";
 
+class Game {
+    constructor({ playersCards, dealersCards, player }) {
+        this.player = player;
+        this.dealer = new Player("Krupier");
+
+        this.playersCards = playersCards;
+        this.dealersCards = dealersCards;
+        this.deck = new Deck();
+        this.deck.shuffle();
+    }
+
+    run() {
+        this.dealCards();
+    }
+
+    dealCards() {
+        for (let n = 0; n < 2; n++) {
+            let card1 = this.deck.pickOne();
+            let card2 = this.deck.pickOne();
+
+            this.player.hand.addCard(card1);
+            this.playersCards.appendChild(card1.render());
+
+            this.dealer.hand.addCard(card2);
+            this.dealersCards.appendChild(card2.render());
+
+            console.log(this.player.hand.cards);
+        }
+    }
+}
+
 const player = new Player("Mateusz");
 
-const deck = new Deck();
-deck.shuffle();
+const game = new Game({
+    player,
+    playersCards: document.getElementById("playersCards"),
+    dealersCards: document.getElementById("dealersCards"),
+});
+
+game.run();
